e9c67d798c440574079d563d792d9af356b2f73b
mjaywoods <mjaywoods@gmail.com>
2013-02-06 22:20:46 +0000

    Add framework so far

---
+++ config.rb
@@ -0,0 +1,30 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "themes/docs/css"
+sass_dir = "themes/docs/_sass"
+images_dir = "themes/docs/img"
+javascripts_dir = "themes/docs/js"
+
+output_style = :expanded
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+preferred_syntax = :scss
+
+
+sass_options = {:debug_info=>false} # by Compass.app 
+
